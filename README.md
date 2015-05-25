313e9c4c599c3396e862c9e68827583328738943
rohitcj <rohit.jhangiani@gmail.com>
2015-05-25 01:34:56 -0600

    Twitter
    Twitter

---
+++ README.md
@@ -0,0 +1,29 @@
+Twitter<br/><br/>
+
+This is a basic twitter app to read and compose tweets the Twitter API.<br/><br/>
+
+Time spent: 16 hours<br/><br/>
+
+Features<br/><br/>
+
+Required<br/>
+[x] User can sign in using OAuth login flow<br/>
+[x] User can view last 20 tweets from their home timeline<br/>
+[x] The current signed in user will be persisted across restarts<br/>
+[x] In the home timeline, user can view tweet with the user profile picture, username, tweet text, and timestamp. In other words, design the custom cell with the proper Auto Layout settings. You will also need to augment the model classes.<br/>
+[x] User can pull to refresh<br/>
+[x] User can compose a new tweet by tapping on a compose button.<br/>
+[x] User can tap on a tweet to view it, with controls to retweet, favorite, and reply.<br/>
+[x] User can retweet, favorite, and reply to the tweet directly from the timeline feed.<br/><br/>
+
+Optional<br/>
+[x] When composing, you should have a countdown in the upper right for the tweet limit.<br/>
+[x] After creating a new tweet, a user should be able to view it in the timeline immediately without refetching the timeline from the network.<br/>
+[x] Retweeting and favoriting should increment the retweet and favorite count.<br/>
+[x] User should be able to unretweet and unfavorite and should decrement the retweet and favorite count.<br/>
+[x] Replies should be prefixed with the username and the reply_id should be set when posting the tweet,<br/>
+[x] User can load more tweets once they reach the bottom of the feed using infinite loading similar to the actual Twitter client.<br/><br/>
+
+Walkthrough
+![alt tag](https://github.com/rohitcj/Twitter/blob/master/TwitterDemo.gif)
+
---
+++ Twitter/User.swift
@@ -0,0 +1,59 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by Rohit Jhangiani on 5/20/15.
+//  Copyright (c) 2015 5TECH. All rights reserved.
+//
+
+import UIKit
+
+var _currentUser: User?
+let currentUserKey = "kCurrentUserKey"
+let userDidSigninNotification = "userDidSigninNotification"
+
+class User: NSObject {
+    var name: String?
+    var screenname: String?
+    var profileImageUrl: String?
+    var tagline: String?
+    var dictionary: NSDictionary
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        name = dictionary["name"] as? String
+        screenname = dictionary["screen_name"] as? String
+        profileImageUrl = dictionary["profile_image_url"] as? String
+        tagline = dictionary["description"] as? String
+    }
+    
+    func signout() {
+        User.currentUser = nil
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        NSNotificationCenter.defaultCenter().postNotificationName(userDidSigninNotification, object: nil)
+        
+    }
+    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil {
+                var data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+                if data != nil {
+                    var dictionary = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as! NSDictionary
+                    _currentUser = User(dictionary: dictionary)
+                }
+        }
+            return _currentUser
+        }
+        set(user) {
+            _currentUser = user
+            if _currentUser != nil {
+                var data = NSJSONSerialization.dataWithJSONObject(user!.dictionary, options: nil, error: nil)
+                NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+            } else {
+                NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+            }
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+}
